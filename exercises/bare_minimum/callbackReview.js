5c3240c09510a12239fb44ef6e2ac08607d08399
kle08 <kevinle1991@gmail.com>
2019-04-30 19:44:56 -0700

    implemented promises and call back review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,46 @@
 
 var fs = require('fs');
 var request = require('request');
-
+// I - filePath
+// O - first file in the filepath
+// C - O(1)
+// E - none 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
-};
+  // gain access to the filepath and have a callback with 2 paramter of err and the data 
+  fs.readFile(filePath, (err, data) => {
+    // if there is an err return callback saying that there is an error 
+    if(err) {
+      callback(err)
+    } else {
+      // if the file exist retrive that file and return the file with a callback
+      // since the data has multible files we have to convert to a string and split it by each time and return the first line
+        callback(err, data.toString().split('\n')[0]);
+    }
+  });
+ 
 
+
+};
+// I - URL
+// O - retive status code of GET request
+// C - O(1)
+// E - none 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+// request url and callback
+request(url, (err, response) => {
+  if (err) {
+    callback(err);
+  } else {
+    callback(null, response.statusCode)
+  }
+});
+// if err (if the file doesnt exist )
+// return err with a call back
+// else if the file exist call back with the status code 
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,36 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
-
+// create a new promist constructor 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  var results = new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString().split('\n')[0]);
+      }
+    });
+  });
+   return results;
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  var results = new Promise((resolve, reject) => {
+    request(url, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        // the reason why there is no null becuaes its not required since it is not a callback function
+        resolve(response.statusCode)
+      }
+    });
+  });
+  return results;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
